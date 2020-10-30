4e2128a94cde94ffded9e73c8772a731a99d658f
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-30 15:20:28 +0100

    Inizio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*
+Descrizione
+Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare attraverso il for..in tutte le proprietà.
+Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente all'array creato al punto 2 inserendo nell’ordine: nome, cognome e età.
+*/
+console.log('js ok');
