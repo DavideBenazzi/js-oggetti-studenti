91a72e78208d154f519f843d0120a09c52f1613c
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-30 15:27:26 +0100

    Oggetto studente

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, co
 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente all'array creato al punto 2 inserendo nell’ordine: nome, cognome e età.
 */
-console.log('js ok');
+
+//OGGETTO STUDENTE
+var studente = {
+  nome : 'Davide',
+  cognome : 'Benazzi',
+  età : 40,
+}
+//STAMPA PROPRIETA'
+for (var key in studente) {
+  console.log(key);
+  console.log(studente[key]);
+}
